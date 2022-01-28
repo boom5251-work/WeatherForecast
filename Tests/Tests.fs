bc5a436af21a70ea0dd46e0d4fd33ebdd27f5351
Виктор Куликов <boom5251@inbox.ru>
2022-01-28 17:57:56 +0300

    Добавлен проект unit-тестирования.

---
+++ Tests/Tests.fs
@@ -0,0 +1,10 @@
+namespace Tests
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+[<TestClass>]
+type TestClass () =
+
+    [<TestMethod>]
+    member this.TestMethodPassing () =
+        Assert.IsTrue(true);
